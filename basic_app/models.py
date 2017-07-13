b46e4f36b9f75a4f4951854150db5cf374dcfb77
jamestthompson3 <jamestthompson3@gmail.com>
2017-07-12 20:36:55 -0700

    fixed user creation form

--- basic_app/models.py
+++ basic_app/models.py
@@ -16,6 +16,5 @@ class UserProfileInfo(models.Model):
     user=models.OneToOneField(User)
     first_name = models.CharField(max_length = 40)
     last_name = models.CharField(max_length = 40)
-    # date_joined = models.DateTimeField(default=timezone.now)
     def __str__(self):
         return self.user.username
